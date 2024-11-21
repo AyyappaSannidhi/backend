771a6ad58547b62f2348645ca2a1b0e3fbc36a69
Kiran Avinash <kiranavinash.sode@gmail.com>
2024-11-21 12:41:05 +0530

    Added proper role

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Lambda Function without the layer (Handled in the deploy.yml file)
+# Create an IAM Role for Lambda with a policy
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach Basic Execution Policy to IAM Role
+resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda Function resource
 resource "aws_lambda_function" "sasss_backend" {
   function_name    = "sasss_backend"
   handler          = "app.handler"
   runtime          = "python3.12"
-  role             = "lambda_execution_role"
+  role             = aws_iam_role.lambda_execution_role.arn  # Use the ARN of the IAM role here
   filename         = "function.zip"
 
   environment {
@@ -15,12 +38,6 @@ resource "aws_lambda_function" "sasss_backend" {
       ENV = "production"
     }
   }
-}
-
-# Attach Basic Execution Policy to IAM Role
-resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
-  role       = "lambda_execution_role"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 output "lambda_function_name" {
